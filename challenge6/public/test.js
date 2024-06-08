3ae85aedb28a5b1f7e6b47e45208eb7d6935fb23
jiyun <ranji12@naver.com>
2024-06-08 20:34:52 +0900

    indexOf test

---
+++ challenge6/public/test.js
@@ -0,0 +1,12 @@
+const array = ["a","b"];
+
+const a = array.indexOf("a");
+const b = array.indexOf("b");
+
+if(a>b){
+    console.log("a가 b보다 인덱스가 더 크다.")
+}
+else{
+    console.log("b가 a보다 인덱스가 더 크다.")
+}
+
