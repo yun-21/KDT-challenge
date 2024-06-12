2460a780d66c3be8d3d5821d8660874d2236f437
shinjiyun <ranji12@naver.com>
2024-06-12 17:07:18 +0900

    class생성자함수 component추가해봤음

--- challenge6/app.js
+++ challenge6/app.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const server = require("./public/serverFile");
-const qs = require("querystring")
+const qs = require("querystring");
+const names = require("./public/name")
 
 
 const servers = http.createServer((request, response) => {
@@ -43,27 +44,82 @@ const servers = http.createServer((request, response) => {
                 const title = str.get("title");
                 const email = str.get("email");
                 const phone = str.get("phone");
-                const jsonData = {
-                    name: title,
-                    email: email,
-                    phone: phone
-                };
-                // fs.writeFileSync("./public/jsonData.json", JSON.stringify(jsonData, null, 2));
-                const component = (tagName, inText) => {
-                    return `<${tagName}>${inText}</${tagName}>`
+                class InnerData{
+                    constructor(){
+                        this.title = title;
+                        this.email = email;
+                        this.phone = phone;
+                    }
+                    set title(value){
+                        if(typeof(value)==='string'){
+                            this._title=value;
+                        }else{
+                            throw new Error("이름이 문자열이 아닙니다.")
+                        }
+                    }
+                    get title(){
+                        return this._title;
+                    }
+
+                    set email(value){
+                        if(typeof(value)==='string'){
+                            this._email=value;
+                        }else{
+                            throw new Error("이메일이 문자열 아닙니다.")
+                        }
+                    }
+                    get email(){
+                        return this._email;
+                    }
                 }
                 const app = () =>{
+                    const inData = new InnerData();
                     function inFunc(){
-                        const arr=[];
-                        for(let key in jsonData){
-                            arr.push(jsonData[key]);
+                        if(inData.title !== ''){
+                            return `<h1>당신의 이름은 <span style="color:blue">${inData.title}</span>이군요</h1>`
                         }
-                        return component('h1',arr)
+                        else{
+                            return `<h1>당신은 이름이 없네요.</h1>`
+                        }
                     }
-                    return inFunc()
+                    function onFunc(){
+                        if(inData.email !== ''){
+                            const gol = inData.email.split('').indexOf('@');
+                            const a =inData.email.split('').splice(gol+1)
+                            const dot = a.indexOf('.');
+                            a.splice(dot);
+                            if(a.join('')==='naver'){
+                                return `<h1>당신은 <span style="color:blue">네이버</span>를 사용하시는군요</h1>`
+                            }
+                            else if(a.join('')==='hanmail'){
+                                return `<h1>당신은 <span style="color:blue">다음</span>을 사용하시는군요</h1>`
+                            }
+                            else if(a.join('')==='kakao'){
+                                return `<h1>당신은 <span style="color:blue">카카오</span>를 사용하시는군요</h1>`
+                            }
+                            else if(a.join('')==='gmail'){
+                                return `<h1>당신은 <span style="color:blue">구글</span>을 사용하시는군요</h1>`
+                            }
+                            else{
+                                return `<h1>어디 메일을 사용하시는건가요?</h1>`
+                            }
+                        }
+                    }
+                    function phFunc(){
+                        const a = inData.phone.split('')
+                        a.splice(inData.phone.split('').indexOf('-'))
+                        if(a.join('')==='010'){
+                            return `<h2>${a.join('')}을 사용하시네요</h2>`
+                        }else if(a.join('')==='011'){
+                            return `<h2>${a.join('')}? 예전 번호를 계속 사용하시나봐요</h2>`
+                        }else{
+                            return `<h2>핸드폰 번호를 제대로 작성해주세요</h2>`
+                        }
+                    }
+
+                    return inFunc()+onFunc()+phFunc()
                 }
-                const htmlData = app()
-                response.write(htmlData);
+                response.write(app());
                 response.end();
             });
         }
